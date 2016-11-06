7dc07a69c510e13c9f2cc8c322174bdfeb20d0c8
NicolasVatel <nicolas.vatel@free.fr>
2016-11-07 00:05:39 +0100

    jeu

---
+++ src/exercice1/modele/Etat.java
@@ -0,0 +1,10 @@
+package tdtp7.exercice1.modele;
+
+/**
+ * Type enumere fortement type representant les deux etats possibles
+ * d'une cellule.
+ *
+ * @note L'ordre d'enumeration ne doit pas etre modifie.
+ */
+public enum Etat { Morte, Vivante };
+
